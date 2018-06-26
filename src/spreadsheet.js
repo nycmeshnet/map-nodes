1395b0103feb5301fb7c669913fa28407c0c2032
Brian Hall <bh@usa.net>
2018-06-26 01:29:49 -0400

    Not Interested

--- src/spreadsheet.js
+++ src/spreadsheet.js
@@ -186,10 +186,15 @@ function removeAbandoned(feature) {
   //added Unsubscribe bh
   if (
     feature.properties.status == "Abandoned" ||
+    feature.properties.status == "Not Interested" ||
     feature.properties.status == "Unsubscribe"
   )
     return false;
   return true;
+
+function removeNoStatus(feature) {
+  if (!feature) return false
+  return true
 }
 
 function generateLinks() {
